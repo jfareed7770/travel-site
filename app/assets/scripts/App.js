389ad69102e0c4b516d45516115291d295d45b72
Juveria <jfareed7770@gmail.com>
2018-09-26 16:33:01 -0400

    Completed sticky header functionality

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -11,7 +11,9 @@ mary.greet();
 import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
 import $ from "jquery";
+import StickyHeader from "./modules/StickyHeader";
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"),"85%");
 new RevealOnScroll($(".testimonial"),"60%");
+var stickyHeader = new StickyHeader();
